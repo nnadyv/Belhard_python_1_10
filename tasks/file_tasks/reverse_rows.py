774fc23e1a91e7f6ad94e26b2542ffc09440b7f7
nadya <naah@gmail.com>
2022-09-04 16:02:47 +0200

    did file_tasks

--- tasks/file_tasks/reverse_rows.py
+++ tasks/file_tasks/reverse_rows.py
@@ -3,4 +3,28 @@
 файл. Создать новый файл, каждая строка которого получается из соответствующей
 строки исходного файла перестановкой слов в обратном порядке.
 """
-import
+from pathlib import Path
+
+
+def revert_rows(text, new_text):
+
+    with open(text, "r+") as t, open(new_text, "w") as t_new:
+        for a in t:
+            t_new.writelines(right_rows(a) + "\n")
+
+
+def right_rows(line):
+    line = line.split()
+    new_dict = {}
+    for idx, a in enumerate(line):
+        if a[-1] in ",.":
+            new_dict[idx] = a[-1]
+        line[idx] = a.strip(",.")
+    line.reverse()
+    for i, l in new_dict.items():
+        line[i] += l
+    line = " ".join(line)
+    return ". ".join(map(lambda v: v.strip().capitalize(), line.split(".")))
+
+
+revert_rows(Path("text.txt"), Path("text1.txt"))
